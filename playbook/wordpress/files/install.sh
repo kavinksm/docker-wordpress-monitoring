226e4707f4651dca4e5fe39a5ee5037853982f44
kavinkumar <kavinks@gmail.com>
2017-07-22 21:01:00 +0530

    playbook - install wordpress, prometheus, grafana with docker

---
+++ playbook/wordpress/files/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker $(whoami)
+sudo apt-get -y install python-pip
+sudo pip install docker-compose
+#sudo apt-get install git
+#sudo mkdir -p /docker/
+#git clone https://github.com/kavinksm/docker-wordpress-monitoring.git /docker/
+#cd /docker/wordpress
+echo "Test playbook run"
